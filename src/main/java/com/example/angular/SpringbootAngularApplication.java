927d4d349cc396fb1d391dafae88d8a27dd04879
Alok Kulkarni <kulkarni.alok@gmail.com>
2017-03-22 11:36:00 +0530

    first commit

---
+++ src/main/java/com/example/angular/SpringbootAngularApplication.java
@@ -0,0 +1,12 @@
+package com.example.angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAngularApplication.class, args);
+	}
+}
